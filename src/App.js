61901a0a2f3746f669c3127bdfa3b9311419c5ed
myusername <sacheth@gmail.com>
2023-05-30 23:50:57 -0400

    integrating tailwind css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
 
@@ -7,7 +7,7 @@ function App() {
   const showEmployees = true;
 
   return (
-    <div className="App">
+    <div className="App bg-red-300">
       {showEmployees ? (
         <>
         <input 
@@ -15,7 +15,7 @@ function App() {
           onChange={(e) => {console.log(e.target.value); setRole(e.target.value)}}/>
           <Employee name=" Shivnath" role = "CEO and Founder"/>
           <Employee name =" Mihir" role={role}/>
-          <Employee name = " Divyasiddha" />
+          <Employee name = " Divvyasiddha" />
         </>
       ): (
         <p>You cannot see the employees</p>
