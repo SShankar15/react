b8a74c023eb7e2108cdfe670431c9c7df305d32f
myusername <sacheth@gmail.com>
2023-05-30 23:36:35 -0400

    useState with 3 employees

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import './App.css';
+import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
+  const [role, setRole] = useState('dev');
+  const showEmployees = true;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello there!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showEmployees ? (
+        <>
+        <input 
+          type ='text' 
+          onChange={(e) => {console.log(e.target.value); setRole(e.target.value)}}/>
+          <Employee name=" Shivnath" role = "CEO and Founder"/>
+          <Employee name =" Mihir" role={role}/>
+          <Employee name = " Divyasiddha" />
+        </>
+      ): (
+        <p>You cannot see the employees</p>
+      )}
     </div>
   );
 }
